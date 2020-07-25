2d94352964c185a929efd4ddceab1265a64346e8
hata <mh.mundus@gmail.com>
2020-07-25 23:46:09 +0900

    7/25

--- test_app1/test8.py
+++ test_app1/test8.py
@@ -22,3 +22,36 @@ print(s)
 equ = "All animals are equal."
 equ = equ.replace("a", "@")
 print(equ)
+
+# 文字を探す
+ani = "animals".index("m")
+print(ani)
+
+try:
+    anime = "anime".index("z")
+    print(anime)
+except:
+    print("Not found.")
+
+# 包含
+azu = "Cat" in "Cat in the hat."
+print(azu)
+azu = "Bat" in "Cat in the hat."
+print(azu)
+
+pot = "Potter" not in "Harry"
+print(pot)
+pot = "Potter" not in "HarryPotter"
+print(pot)
+
+# エスケープ文字
+bot = "彼女は\"そうだね\"と言った"
+print(bot)
+bot = "彼女は'そうだね'と言った"
+print(bot)
+bot = '彼女は"そうだね"と言った'
+print(bot)
+
+# 改行
+line = "line1\nline2\nline3"
+print(line)
