095b16e1eadbda53321e0f59515959d02595a41e
hata <mh.mundus@gmail.com>
2020-08-04 10:42:15 +0900

    8/4

--- test_app6_MoreObject/challenge.py
+++ test_app6_MoreObject/challenge.py
@@ -6,8 +6,17 @@ class Square:
         self.len = l
         self.square_list.append((self.width, self.len))
 
+    def __repr__(self):
+        return "{} by {} by {} by {}".format(self.width,self.width,self.width,self.width)
+
 sq1 = Square(5,12)
 sq2 = Square(7,18)
 sq3 = Square(9,29)
+
+print(Square.square_list)
+
+sq4 = Square(29,1)
+
+print(sq4)
 
 print(Square.square_list)
