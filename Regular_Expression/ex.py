5f264c61f72fc9c044bc41a9876a69fd29793b19
hata <mh.mundus@gmail.com>
2020-08-04 14:03:15 +0900

    8/4

--- Regular_Expression/ex.py
+++ Regular_Expression/ex.py
@@ -28,3 +28,39 @@ print(m)
 line = "123 hi 34 hello."
 m = re.findall("\d", line, re.IGNORECASE)
 print(m)
+
+# 繰り返し
+# (非貪欲な正規表現)
+t = "__one__ __two__ __three__"
+found = re.findall("__.*?__", t)
+for match in found:
+    print(match)
+
+# Mad Libsゲーム
+# 入力された内容を虫食いに入れて表示させる
+text = """キリンは大昔から __職業__ の興味の対象でした。
+キリンは __〇〇類__ の中で一番背が高いですが、科学者たちはそのような長い __体の一部__ をどうやって
+獲得したのか説明できませんでした。キリンの身長は __数値__ __単位__ 近くあり、その高さのほとんどは脚と
+ __体の一部__ によるものです。
+"""
+def mad_libs(mls):
+    hints = re.findall("__.*?__", mls)
+    if hints is not None:
+        for word in hints:
+            q = "{} を入力:".format(word)
+            new = input(q)
+            mls = mls.replace(word, new, 1)
+        print('\n')
+        mls = mls.replace("\n", "")
+        print(mls)
+    else:
+        print("引数mlsが無効です")
+
+mad_libs(text)
+
+# エスケープ
+# (例えば $ は、行の終端に一致するパターンを作るが、
+# エスケープすることで、ドル記号そのものに一致する)
+line = "I love $ money"
+m = re.findall("\\$", line, re.IGNORECASE)
+print(m)
