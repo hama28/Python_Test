32bf3e29525f9cb1edb87779ed16277c546953c3
hata <mh.mundus@gmail.com>
2020-08-05 10:43:29 +0900

    8/5 scraping fin

--- Scraping/web_scraping.py
+++ Scraping/web_scraping.py
@@ -14,17 +14,19 @@ class Scraper:
 
         now = datetime.datetime.now()
         print(now.strftime('%Y年%m月%d日%H時%M分%S秒'))
+
+        fav = 400
         
-        with open("test.csv", "w", newline='') as f:
+        with open("test.csv", "a", newline='') as f:
             w = csv.writer(f)
-            w.writerow(["取得日：" + now.strftime('%Y年%m月%d日%H時%M分%S秒')])
+            w.writerow(["取得時間：" + now.strftime('%Y年%m月%d日%H時%M分%S秒')])
             for div in soup.select("div.entrylist-contents-main"):
                 title = div.h3
                 url = div.a
                 user = div.span
                 user_num = user.getText().split(" ")
             
-                if int(user_num[0]) >= 400:
+                if int(user_num[0]) >= fav:
                     print(title.getText())
                     print(url.get('href'))
                     print(user.getText())
